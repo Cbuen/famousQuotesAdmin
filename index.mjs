dea685ab96555a3766127f2ad80a3eb8318c4729
Chris <chrismightdev@gmail.com>
2026-08-03 23:01:34 -0700

    initial commit

---
+++ index.mjs
@@ -0,0 +1,227 @@
+import express from 'express';
+import mysql from 'mysql2/promise';
+
+//TODO: Add styling and use partials to render some html contnet
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+//for Express to get values using POST method
+app.use(express.urlencoded({ extended: true }));
+
+//setting up database connection pool
+const conn = mysql.createPool({
+  host: "sql3.freesqldatabase.com",
+  user: "sql3834238",
+  password: "U9NjYstPZZ",
+  database: "sql3834238",
+  connectionLimit: 10,
+  waitForConnections: true
+});
+
+//routes
+app.get('/', (req, res) => {
+  res.render('index')
+});
+
+
+app.get('/author/new', (req, res) => {
+  res.render('newAuthor')
+});
+
+app.post("/author/new", async function (req, res) {
+  let fName = req.body.fName;
+  let lName = req.body.lName;
+  let birthDate = req.body.birthDate;
+  let sql = `INSERT INTO q_authors
+             (firstName, lastName, dob)
+              VALUES (?, ?, ?)`;
+  let params = [fName, lName, birthDate];
+  const [rows] = await conn.query(sql, params);
+  res.render("newAuthor",
+    { "message": "Author added!" });
+});
+
+app.get("/authors", async function (req, res) {
+  let sql = `SELECT *
+            FROM q_authors
+            ORDER BY lastName`;
+  const [rows] = await conn.query(sql);
+  res.render("authorList", { "authors": rows });
+});
+
+app.get("/author/edit", async function (req, res) {
+
+
+  let authorId = req.query.authorId;
+
+
+  let sql = `SELECT *, 
+        DATE_FORMAT(dob, '%Y-%m-%d') dobISO
+        FROM q_authors
+        WHERE authorId =  ${authorId}`;
+  const [rows] = await conn.query(sql);
+  res.render("editAuthor", { "authorInfo": rows });
+});
+
+
+app.post("/author/edit", async function (req, res) {
+  let sql = `UPDATE q_authors
+            SET firstName = ?,
+                lastName = ?,
+                dob = ?,
+                sex = ?
+            WHERE authorId =  ?`;
+
+
+  let params = [req.body.fName,
+  req.body.lName, req.body.dob,
+  req.body.sex, req.body.authorId];
+  const [rows] = await conn.query(sql, params);
+  res.redirect("/authors");
+});
+
+app.get('/author/delete', async function (req, res) {
+  let authorId = req.query.authorId;
+
+  let sql = `DELETE FROM q_authors WHERE authorId = ?`;
+
+  const [rows] = await conn.query(sql, [authorId]);
+
+  res.redirect("/authors");
+});
+
+app.get("/quotes", async function (req, res) {
+  let sql = `SELECT *
+            FROM q_quotes
+            ORDER BY quoteId`;
+
+  const [rows] = await conn.query(sql);
+
+  res.render("quoteList", { "quotes": rows });
+});
+
+app.get("/quote/new", async function (req, res) {
+
+  let sql = `SELECT *
+            FROM q_authors
+            ORDER BY lastName`;
+
+  const [authors] = await conn.query(sql);
+
+  sql = `SELECT DISTINCT category
+        FROM q_quotes
+        ORDER BY category`;
+
+  const [categories] = await conn.query(sql);
+
+  res.render("newQuote", {
+    "authors": authors,
+    "categories": categories
+  });
+
+});
+
+app.post("/quote/new", async function (req, res) {
+
+  let sql = `INSERT INTO q_quotes
+            (quote, authorId, category, likes)
+            VALUES (?, ?, ?, ?)`;
+
+  let params = [
+    req.body.quote,
+    req.body.authorId,
+    req.body.category,
+    req.body.likes
+  ];
+
+  await conn.query(sql, params);
+
+  sql = `SELECT *
+        FROM q_authors
+        ORDER BY lastName`;
+
+  const [authors] = await conn.query(sql);
+
+  sql = `SELECT DISTINCT category
+        FROM q_quotes
+        ORDER BY category`;
+
+  const [categories] = await conn.query(sql);
+
+  res.render("newQuote", {
+    "authors": authors,
+    "categories": categories,
+    "message": "Quote added!"
+  });
+
+});
+
+
+app.get("/quote/edit", async function (req, res) {
+
+  let quoteId = req.query.quoteId;
+
+  let sql = `SELECT *
+            FROM q_quotes
+            WHERE quoteId = ?`;
+
+  const [rows] = await conn.query(sql, [quoteId]);
+
+  res.render("editQuote", { "quoteInfo": rows });
+});
+
+
+app.post("/quote/edit", async function (req, res) {
+
+  let sql = `UPDATE q_quotes
+            SET quote = ?,
+                authorId = ?,
+                category = ?,
+                likes = ?
+            WHERE quoteId = ?`;
+
+  let params = [req.body.quote,
+  req.body.authorId,
+  req.body.category,
+  req.body.likes,
+  req.body.quoteId];
+
+  const [rows] = await conn.query(sql, params);
+
+  res.redirect("/quotes");
+});
+
+
+app.get("/quote/delete", async function (req, res) {
+
+  let quoteId = req.query.quoteId;
+
+  let sql = `DELETE FROM q_quotes
+            WHERE quoteId = ?`;
+
+  const [rows] = await conn.query(sql, [quoteId]);
+
+  res.redirect("/quotes");
+});
+
+
+
+app.get("/dbTest", async (req, res) => {
+  try {
+    const [rows] = await pool.query("SELECT CURDATE()");
+    res.send(rows);
+  } catch (err) {
+    console.error("Database error:", err);
+    res.status(500).send("Database error");
+  }
+});//dbTest
+
+let PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Express server running on port ${PORT}`);
+});
+
+
